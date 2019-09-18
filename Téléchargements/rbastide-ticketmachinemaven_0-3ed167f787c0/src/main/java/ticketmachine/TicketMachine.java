1038d2bc1a367cc97c0d003e9aa505c13e3b1e0a
ajalaber <ajalaber@pcalb-mm0514.univ-jfc.fr>
2019-09-18 14:33:05 +0200

    Modification méthode insertMoney

--- Téléchargements/rbastide-ticketmachinemaven_0-3ed167f787c0/src/main/java/ticketmachine/TicketMachine.java
+++ Téléchargements/rbastide-ticketmachinemaven_0-3ed167f787c0/src/main/java/ticketmachine/TicketMachine.java
@@ -63,7 +63,10 @@ public class TicketMachine {
 	 * @param amount the amount inserted, in cents (positive)
 	 * @throws IllegalArgumentException if amount is not positive
 	 */
-	public void insertMoney(int amount) {
+	public void insertMoney(int amount) throws IllegalArgumentException {
+                if(amount < 0) {
+                    throw new IllegalArgumentException();
+                }
 		balance = balance + amount;
 	}
 
