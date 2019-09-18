0b9fe886d7fb2e1176309844bacc256b1d91654e
ajalaber <ajalaber@pcalb-mm0514.univ-jfc.fr>
2019-09-18 14:09:06 +0200

    Test

--- Téléchargements/rbastide-ticketmachinemaven_0-3ed167f787c0/src/main/java/ticketmachine/Main.java
+++ Téléchargements/rbastide-ticketmachinemaven_0-3ed167f787c0/src/main/java/ticketmachine/Main.java
@@ -3,7 +3,7 @@ package ticketmachine;
 public class Main {
 
 	public static void main(String[] args) {
-		TicketMachine machine = new TicketMachine(50);
+		TicketMachine machine = new TicketMachine(40);
 		System.out.println("L'utilisateur insère 60 centimes");
 		machine.insertMoney(60);
 		System.out.println("L'utilisateur appuie sur 'Impression ticket'");
